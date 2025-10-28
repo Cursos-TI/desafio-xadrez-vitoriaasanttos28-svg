6ccbffd776cbd6c8e4954cf38474c793fd1069e2
vitoriaasanttos28-svg <227267038+vitoriaasanttos28-svg@users.noreply.github.com>
2025-10-28 21:00:33 +0000

    movimentaçoes das peças

--- xadrez.c
+++ xadrez.c
@@ -7,16 +7,25 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+int i = 0;
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    
+while (i< 5){
+    printf(" cima,direita\n");
+    i++;
+}
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    
+for (int i = 0; i < 5; i++){
 
+ printf("Direita\n");
+    }
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    
+    //bloco de codigo
+    do {
+        printf("Esquerda\n");
+}while (i < 8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
